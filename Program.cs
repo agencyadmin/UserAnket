a04090f27b4b1d1a663971dbaa893cb8023fbfc3
Name <osmprobox@gmail.com>
2021-11-28 14:41:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		
+	Console.WriteLine("What is your name?");
+		String MyName = Console.ReadLine();
+		Console.WriteLine("What is your age?");
+		Byte MyAge = (byte) int.Parse(Console.ReadLine());
+	    Console.WriteLine("Your name is {0} and your age is {1}", MyName, MyAge);
+		Console.WriteLine("What is your birthday?");
+		String Birthday = Console.ReadLine();
+		Console.WriteLine($"Your birthdate is {Birthday}");
+	}	
+}
